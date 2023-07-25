433c474b70431e8b8c432bd5742bec39dd2227fd
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-07-25 09:53:39 +0200

    Transactions fix

--- service/schemas/validation.js
+++ service/schemas/validation.js
@@ -1,8 +1,4 @@
 const Joi = require("joi").extend(require("@joi/date"));
-const {
-  transactionCategories,
-  transactionTypes,
-} = require("../variables/transactionVariables");
 
 const userSignupValidationSchema = Joi.object({
   email: Joi.string().email().required(),
@@ -16,8 +12,22 @@ const userLoginValidationSchema = Joi.object({
 });
 
 const transactionValidationSchema = Joi.object({
-  type: Joi.string().valid(transactionTypes.toString()).required(),
-  category: Joi.string().valid(transactionCategories.toString()).required(),
+  type: Joi.string().valid("Income", "Expense").required(),
+  category: Joi.string()
+    .valid(
+      "Income",
+      "Main expenses",
+      "Products",
+      "Car",
+      "Self care",
+      "Child care",
+      "Household products",
+      "Education",
+      "Leisure",
+      "Other expenses",
+      "Entertainment"
+    )
+    .required(),
   value: Joi.number().required(),
   date: Joi.date().format("MM-DD-YYYY").required(),
   comment: Joi.string(),
